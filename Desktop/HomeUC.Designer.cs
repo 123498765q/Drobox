b10236e748978f82c149f2e24ae0ab280c219ac8
zarembolt <tomas.zaremba@gmail.com>
2018-03-20 10:11:51 +0200

    Sutvarkyti nenaudojami user screenai

---
+++ Desktop/HomeUC.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Desktop
+{
+    partial class HomeUC
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.addFile = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.addFile);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(737, 382);
+            this.panel1.TabIndex = 1;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(285, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // addFile
+            // 
+            this.addFile.FlatAppearance.BorderSize = 0;
+            this.addFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addFile.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.addFile.ForeColor = System.Drawing.Color.White;
+            this.addFile.Image = global::Desktop.Properties.Resources.prideti;
+            this.addFile.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.addFile.Location = new System.Drawing.Point(662, 316);
+            this.addFile.Name = "addFile";
+            this.addFile.Size = new System.Drawing.Size(40, 40);
+            this.addFile.TabIndex = 9;
+            this.addFile.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.addFile.UseVisualStyleBackColor = true;
+            this.addFile.Click += new System.EventHandler(this.addFile_Click);
+            // 
+            // HomeUC
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "HomeUC";
+            this.Size = new System.Drawing.Size(737, 382);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
---
+++ Desktop/HomeUC.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+
+
+namespace Desktop
+{
+    public partial class HomeUC : UserControl
+    {
+        public HomeUC()
+        {
+            InitializeComponent();
+            this.panel1.AutoScroll = true;
+
+            DirectoryInfo d = new DirectoryInfo(@"C:\Users\ASUS-pc\Desktop\Cloud");
+            FileInfo[] Files = d.GetFiles();
+            string str = "";
+            Point newLoc = new Point(50, 50);
+          
+            string[] t;
+            int i = 0;
+            int RowCount = 3;
+
+            //foreach (FileInfo file in Files)
+            //{
+            //    str = str + ", " + file.Name;
+            //    int column = 0;
+            //    for (; column < RowCount; column++)
+            //    {
+            //        string fileExt = System.IO.Path.GetExtension(file.ToString());
+            //        Button button = new Button();
+            //        button.Text = "valio";//Files[i].ToString();
+            //        //button.Left = column * 100;
+            //        button.Top = column * 120;
+            //        button.Width = 50;
+            //        button.Height = 50;
+            //        button.Location = new Point(column * 80, i * 120);
+            //        button.TextImageRelation = TextImageRelation.ImageAboveText;
+            //        button.TabStop = false;
+            //        button.FlatStyle = FlatStyle.Flat;
+            //        button.FlatAppearance.BorderSize = 0;
+            //        //button.TextAlign =
+            //        button.Click += Button_Click;
+
+            //        //if (fileExt == ".txt")
+            //        //{
+            //        //    button.Image = Properties.Resources.txt;
+            //        //}
+            //        //else if (fileExt == ".png")
+            //        //{
+            //        //    button.Image = Image.FromFile(@"C:\Users\ASUS-pc\Desktop\Cloud\dalintis.png");
+            //        //}
+            //        panel1.Controls.Add(button);
+            //    }
+            //    i++;
+            //}
+            
+            for (int y = 0; y < Files.Length; y++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    string fileExt = System.IO.Path.GetExtension(Files[i].ToString());
+                    Button button = new Button();
+                    button.Text = Files[i].ToString();
+                    button.Width = 75;
+                    button.Height = 50;
+                    button.Location = new Point(x * 80, y * 20);
+                    panel1.Controls.Add(button);
+                    button.Click += Button_Click;
+                    if (fileExt == ".txt")
+                    {
+                        button.Image = Properties.Resources.txt;
+                    }
+                    else if (fileExt == ".png")
+                    {
+                        //button.Image = Image.FromFile(@"C:\Users\ASUS-pc\Desktop\Cloud\dalintis.png");
+                    }
+                    i++;
+                }
+                y += 3;   
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+
+            string failas = (sender as Button).Text;
+            System.Diagnostics.Process.Start(@"C:\Users\ASUS-pc\Desktop\Cloud\" + failas);
+        }
+
+        private void Panel1_Scroll(object sender, ScrollEventArgs e)
+        {
+            addFile.Location = new Point(662 + this.AutoScrollPosition.X, 316 + this.AutoScrollPosition.Y);
+        }
+
+        private void addFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fDialog = new OpenFileDialog();
+            if (fDialog.ShowDialog() != DialogResult.OK)
+                return;
+            System.IO.FileInfo fInfo = new System.IO.FileInfo(fDialog.FileName);
+            string strFileName = fInfo.Name;
+            string strFilePath = fInfo.DirectoryName;
+            string targetPath = @"C:\Users\ASUS-pc\Desktop\Cloud";
+            if (!System.IO.Directory.Exists(targetPath))
+                {
+                    System.IO.Directory.CreateDirectory(targetPath);
+                }
+                System.IO.File.Copy(strFilePath + '\\' + strFileName, targetPath + '\\' + strFileName, true);
+                
+            
+        }
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] FileList = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            string strFilePath = FileList[0];
+            string fileName = Path.GetFileName(strFilePath);
+            string targetPath = @"C:\Users\ASUS-pc\Desktop\Cloud";
+            if (!fileName.Contains("."))
+            {
+                CloneDirectory(strFilePath, targetPath);
+            }
+            else
+            {
+                if (!System.IO.Directory.Exists(targetPath))
+                {
+                    System.IO.Directory.CreateDirectory(targetPath);
+                }
+                System.IO.File.Copy(strFilePath, targetPath + '\\' + fileName, true);
+            }
+  
+        }
+
+        private void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private static void CloneDirectory(string root, string dest)
+        {
+            foreach (var directory in Directory.GetDirectories(root))
+            {
+                string dirName = Path.GetFileName(directory);
+                if (!Directory.Exists(Path.Combine(dest, dirName)))
+                {
+                    Directory.CreateDirectory(Path.Combine(dest, dirName));
+                }
+                CloneDirectory(directory, Path.Combine(dest, dirName));
+            }
+            foreach (var file in Directory.GetFiles(root))
+            {
+                File.Copy(file, Path.Combine(dest, Path.GetFileName(file)));
+            }
+        }
+    }
+    }
+
