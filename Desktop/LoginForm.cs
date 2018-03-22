5db6f22554d0163d818375ab140323d7b881fb3c
jogurtas <kisielius.mykolas@gmail.com>
2018-03-22 10:48:49 +0200

    Loginas

--- Desktop/LoginForm.cs
+++ Desktop/LoginForm.cs
@@ -7,8 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using CefSharp;
-using CefSharp.WinForms;
 
 namespace Desktop
 {
@@ -18,19 +16,5 @@ namespace Desktop
         {
             InitializeComponent();
         }
-        private ChromiumWebBrowser chrome;
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            CefSettings settings = new CefSettings();
-            Cef.Initialize(settings);
-            string url = "http://localhost:57769/GoogleLogin/index.html";
-            chrome = new ChromiumWebBrowser(url);
-            this.panel1.Controls.Add(chrome);
-            chrome.Dock = DockStyle.Fill;
-            
-        }
-
-        
     }
 }
