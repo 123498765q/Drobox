180bff29f576646460ebc1a27a49d4f57c3dcbad
Zmx7500 <auriszmx@gmail.com>
2018-04-12 08:17:47 +0300

    PhotosScreeen

--- UWP/MainPage.xaml.cs
+++ UWP/MainPage.xaml.cs
@@ -5,12 +5,15 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -26,6 +29,7 @@ namespace UWP
         {
             this.InitializeComponent();
         }
-        
+
+      
     }
 }
